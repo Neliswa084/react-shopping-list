2191bb9c0aa59534747b6af9dcb944230bbc466b
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-18 15:42:42 +0200

    Created the Modal

---
+++ src/Components/Modals/Modal.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './Modal.module.css'
+import CloseIcon from '../../assets/close.png'
+
+
+type ModalProps ={
+    children: React.ReactNode,
+    close: () => void
+}
+
+export const Modal:React.FC<ModalProps> = ({children,close}) => {
+      const stopProgation = (e: React.MouseEvent<HTMLDivElement>) =>{
+    e.stopPropagation()
+  }
+  return (
+    <div className={styles['overlay']} onClick={close}>
+        
+        <div className={styles['overlay-child']} onClick={stopProgation}> 
+           <img className={styles['close-icon']} src={CloseIcon} onClick={close} />
+          {children}
+         </div>
+                
+    </div>
+    
+   
+  )
+}
