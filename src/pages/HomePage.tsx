e09fa7e5db2120bf33a8f1f284a406f9399f4a82
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-19 08:44:19 +0200

    added the navigation to home , and created not found page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navbar } from '../Components/Navbar/Navbar'
+
+export const HomePage = () => {
+  return (
+    <>
+     <Navbar />
+     <h1>WELCOME TO HOME PAGE</h1>
+     </>
+  )
+}
