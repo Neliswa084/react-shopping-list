4dc25f2886cc448522d7a102dfabd6d6f8aed7f0
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-19 12:02:34 +0200

    Added the statistic card on the Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Navbar } from '../Components/Navbar/Navbar'
+import { StatisticCard } from '../Components/UI/StatisticCard/StatisticCard'
+import styles from './HomePage.module.css'
+import { SearchBar } from '../Components/UI/SearchBar/SearchBar'
 
 export const HomePage = () => {
   return (
     <>
+      <div className={styles.container}>
      <Navbar />
-     <h1>WELCOME TO HOME PAGE</h1>
+     <div className={styles['statistic']}>
+     <StatisticCard value={3} title='Lists' />
+     <StatisticCard value={5} title='Categories' />
+      <StatisticCard value={10} title='Items' />
+      <StatisticCard value={4} title='Items' />
+     </div>
+     <SearchBar   value='value' onChange={() => {}} />
+  </div>
      </>
   )
 }
