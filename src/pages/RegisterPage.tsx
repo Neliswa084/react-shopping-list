46b01f4af1b6c6e1994d1fc878bd8e20533cbbf8
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-19 11:35:21 +0200

    Fixed small errors

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -8,15 +8,15 @@ import { Button } from '../Components/UI/Button/Button'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 
-import {registerStart, registerUser ,registerFailure} from '../redux/reducers/signUpSlice'
+import { registerStart, registerUser, registerFailure } from '../redux/reducers/signUpSlice'
 import type { RootState } from '../redux/store'
 
 export const RegisterPage: React.FC = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
-  const  error = useSelector((state:RootState) => state.signUp.error)
-  const loading = useSelector((state:RootState) => state.signUp.loading )
+  const error = useSelector((state: RootState) => state.signUp.error)
+  const loading = useSelector((state: RootState) => state.signUp.loading)
 
 
   const [name, setName] = useState('')
@@ -26,40 +26,40 @@ export const RegisterPage: React.FC = () => {
   const [password, setPassword] = useState('')
   const [confirm, setConfirm] = useState('')
 
-   const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
 
-      if (password !== confirm) {
+    if (password !== confirm) {
       dispatch(registerFailure("Passwords do not match!"))
       return
     }
-        const newUser = { name, surname, email, cellNumber, password }
+    const newUser = { name, surname, email, cellNumber, password }
 
     try {
-      // 1. Alert Redux that the network request is beginning
+
       dispatch(registerStart())
 
-      // 2. Run the asynchronous POST request to your JSON server
+
       const response = await axios.post('http://localhost:3000/users', newUser)
 
-      // 3. Server sends back the saved user containing their new unique ID
+
       dispatch(registerUser(response.data))
 
       alert('Registration successful!')
-      navigate('/login') // Redirect the user to login page
+      navigate('/login')
     } catch (err: any) {
-      // 4. Capture any network or backend crashes
+
       dispatch(registerFailure(err.message || 'Server error occurred.'))
     }
-  
-   }
+
+  }
   return (
     <div className={styles.container}>
       <Card>
         <h2 className={styles.title}>Create account</h2>
         <p className={styles.subtitle}>Join ShoppingList and start organising</p>
-         {error && <p className={styles.errorMessage} style={{ color: 'red' }}>{error}</p>}
-        <form className={styles.form}  onSubmit={handleSubmit}>
+        {error && <p className={styles.errorMessage} style={{ color: 'red' }}>{error}</p>}
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.row}>
             <Input label="Name" value={name} onChange={(e) => setName(e.target.value)} name="name" />
             <Input label="Surname" value={surname} onChange={(e) => setSurname(e.target.value)} name="surname" />
@@ -70,7 +70,7 @@ export const RegisterPage: React.FC = () => {
             <Input label="Password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" />
             <Input label="Confirm" value={confirm} onChange={(e) => setConfirm(e.target.value)} name="confirm" />
           </div>
-          <Button label= {loading ? "Creating": "Create account"} type="submit" />
+          <Button label={loading ? "Creating" : "Create account"} type="submit" />
           <p className={styles.switchText}>
             Already have an account?{' '}
             <span className={styles.link} onClick={() => navigate('/login')}>
