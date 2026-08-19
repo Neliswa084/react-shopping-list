14210c7c0298dbfa8633d617aeb160a5a4b04c32
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-19 02:27:47 +0200

    Done with the Reusable input

---
+++ src/Components/UI/Input/Input.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Input.module.css'
+
+type InputProps ={
+    id?: string,
+    value?: string,
+    onChange: React.ChangeEventHandler<HTMLInputElement>,
+    style?: React.CSSProperties,
+    label: string,
+    error?: string,
+    name?: string
+}
+export const Input:React.FC<InputProps> = ({id, value, onChange, style, label, error, name}) => {
+  return (
+   <div className={styles['input-container']}>
+       <label className={styles['input-label']}>{label}</label>
+       <input name={name} type="text" id={id} style={style} value={value} onChange={onChange} className={styles.input}/>
+       {error && <span className={styles['input-error']}>{error}</span>}
+    </div>
+  )
+}
