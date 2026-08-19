764161e7f1d0a7561747cbc17ef98c59f49dc435
Neliswa084 <neliswangcobo141@gmail.com>
2026-08-19 22:01:43 +0200

    continued with making homw page content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,20 +3,43 @@ import { Navbar } from '../Components/Navbar/Navbar'
 import { StatisticCard } from '../Components/UI/StatisticCard/StatisticCard'
 import styles from './HomePage.module.css'
 import { SearchBar } from '../Components/UI/SearchBar/SearchBar'
+import { Button } from '../Components/UI/Button/Button'
 
 export const HomePage = () => {
   return (
     <>
       <div className={styles.container}>
-     <Navbar />
-     <div className={styles['statistic']}>
+           <Navbar />
+        <div className={styles.content}>
+  
+     <div className={styles.statistic}>
      <StatisticCard value={3} title='Lists' />
      <StatisticCard value={5} title='Categories' />
       <StatisticCard value={10} title='Items' />
       <StatisticCard value={4} title='Done' />
      </div>
-     <SearchBar value='value' onChange={() => {}} />
+
+    <div className={styles.searchRow}>
+  <div className={styles.searchWrapper}>
+     <SearchBar value='' onChange={() => {}} />
+      </div>
+
+      <button className={styles.sortBtn}>Name</button>
+  <button className={styles.sortBtn}>Category</button>
+  <button className={styles.sortBtn}>Date</button>
+  <div className={styles.addBtn}>
+    <Button label="+ Add List" onClick={() => {}} />
   </div>
+  </div>
+<h2 className={styles.listsTitle}>My Shopping Lists</h2>
+<div className={styles.grid}>
+  
+</div>
+  
+</div>
+  </div>
+
+
      </>
   )
 }
